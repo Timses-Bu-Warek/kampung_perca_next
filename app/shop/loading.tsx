e836b3b494b93df58b1e47272cf1e30ac845009f
intanpermata11 <intan.permatass1101@gmail.com>
2023-07-28 13:00:32 +0700

    improvement skeleton shop page

--- app/shop/loading.tsx
+++ app/shop/loading.tsx
@@ -5,12 +5,12 @@ export default function Loading() {
     <main>
       <div className="container grid md:grid-cols-4 gap-6 pt-16 pb-16 items-start">
         <div className="col-span-1 bg-white px-4 pb-6">
-          <div className="bg-gray-200 w-full h-10 overflow-hidden rounded-md  p-3 shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/50 hover:shadow-lg before:animate-[shimmer_20.5s_infinite]"></div>
+          <div className="bg-gray-200 w-full h-10 overflow-hidden rounded-md  p-3 shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/50 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]"></div>
         </div>
         <div className="col-span-3">
           <div></div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/50 hover:shadow-lg before:animate-[shimmer_20.5s_infinite]">
-            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 ">
+            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md p-3  group flex justify-between flex-col shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]">
               <div className="h-[300px] w-full rounded-lg bg-gray-200"></div>
               <div className="space-y-3">
                 <div className="h-5 w-8/12 rounded-full bg-gray-200"></div>
@@ -20,7 +20,7 @@ export default function Loading() {
                 </div>
               </div>
             </div>
-            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col">
+            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]">
               <div className="h-[300px] w-full rounded-lg bg-gray-200"></div>
               <div className="space-y-3">
                 <div className="h-5 w-8/12 rounded-full bg-gray-200"></div>
@@ -30,7 +30,7 @@ export default function Loading() {
                 </div>
               </div>
             </div>
-            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col">
+            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]">
               <div className="h-[300px] w-full rounded-lg bg-gray-200"></div>
               <div className="space-y-3">
                 <div className="h-5 w-8/12 rounded-full bg-gray-200"></div>
@@ -40,8 +40,8 @@ export default function Loading() {
                 </div>
               </div>
             </div>
-            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3  group flex justify-between flex-col">
-              <div className="h-[300px] w-full rounded-lg bg-gray-200"></div>
+            <div className="bg-white relative w-60 space-y-3 overflow-hidden rounded-md  p-3 group flex justify-between flex-col shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]">
+              <div className="h-[300px] w-full rounded-lg bg-gray-200 "></div>
               <div className="space-y-3">
                 <div className="h-5 w-8/12 rounded-full bg-gray-200"></div>
                 <div className="space-y-1">
