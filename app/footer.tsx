d48a6132cc9c8f99417e945668e6562f87ea04da
intanpermata11 <intan.permatass1101@gmail.com>
2023-11-13 15:31:01 +0700

    Update footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -91,16 +91,6 @@ export default function Footer() {
             </form>
           </div>
 
-          {/* <!-- Copyright information --> */}
-          <div className="mb-6">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-              distinctio earum repellat quaerat voluptatibus placeat nam,
-              commodi optio pariatur est quia magnam eum harum corrupti dicta,
-              aliquam sequi voluptate quas.
-            </p>
-          </div>
-
           {/* <!-- Links section --> */}
         </div>
 
