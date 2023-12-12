c08505ccc2df97c0658ddd83c7008aae29df247c
intanpermata11 <intan.permatass1101@gmail.com>
2023-12-12 15:28:11 +0700

    Add Sitemap

--- next.config.js
+++ next.config.js
@@ -1,15 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'pakar.stakcdn.com',
-            port: '',
-            pathname: '/app/assets/**',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pakar.stakcdn.com",
+        port: "",
+        pathname: "/app/assets/**",
       },
-}
+    ],
+  },
+};
 
-module.exports = nextConfig
+const siteUrl = "https://kampungperca.id/";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  // tambahkan sitemap ke plugins
+  plugins: [
+    {
+      resolve: "next-sitemap",
+      options: {
+        baseUrl: siteUrl,
+      },
+    },
+  ],
+};
+
+module.exports = nextConfig;
