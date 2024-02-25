b80df92eb93edea76d4101f3b092057dd6a744c8
intanpermata11 <intan.permatass1101@gmail.com>
2024-02-25 18:01:43 +0700

    Update page shop unfixed

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -10,8 +10,43 @@ import Loading from "./shopLoading";
 import { ProductSort } from "./components/ProductSort";
 
 export const metadata: Metadata = {
-  title: "Shop Kampung Perca Products",
-  description: "Beli produk kampung perca",
+  title: "Beli Produk",
+  description:
+    "Beli Produk Perca di Kampung Perca Sindangsari. Murah dan Berkualitas.",
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+  },
+  keywords: [
+    "Jual Alas Mangkuk Perca",
+    "Jual Appron Perca",
+    "Jual Baju Anak Perca",
+    "Jual Baju Pangsi",
+    "Jual Baju Tidur Perca",
+    "Jual Bantal Duduk",
+    "Jual Bantal Kursi",
+    "Jual Blouse Perca",
+    "Jual Cempal Ayam",
+    "Jual Daster",
+    "Jual Dompet Koin",
+    "Jual Gantungan Kunci",
+    "Jual Goodie Bag Perca",
+    "Jual Ikat Rambut",
+    "Jual Keset Bentuk",
+    "Jual Masker Perca",
+    "Jual Lap tangan",
+    "Jual Outer Perca",
+    "Jual Pouch",
+    "Jual Rok Perca",
+    "Jual Rompi Perca",
+    "Jual Baju Tidur Perca",
+    "Jual Totopong",
+    "Jual Notebook",
+    "Jual Tempat tisu perca",
+    "Jual Baju Tidur Perca",
+    "Jual Tas Perca",
+  ],
 };
 
 export default function Shop() {
@@ -260,7 +295,7 @@ export default function Shop() {
           <div className="col-span-3">
             {/* <!-- sorting --> */}
             <div className="flex items-center mb-4">
-            <ProductSort />
+              <ProductSort />
 
               <div className="sm:hidden flex gap-2 ml-auto">
                 <div className="border border-primary w-10 h-9 flex items-center justify-center text-white bg-primary rounded cursor-pointer">
