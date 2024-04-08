df23324d275136e51e35cc4772e96ba2a18e0bf8
Intan Permatasari <intan.permatass1101@gmail.com>
2024-04-09 05:20:10 +0700

    update metadata

--- app/shop/[NamaProduk]/page.tsx
+++ app/shop/[NamaProduk]/page.tsx
@@ -9,15 +9,11 @@ export async function generateMetadata({
     params.NamaProduk.replace("%20", " ")
   );
 
-  // const decodedKeterangan = decodeURIComponent(
-  //   params.Keterangan.replace("%20", " ")
-  // );
-
   return {
     title: `${decodedNamaProduk}`,
     description: `Keterangan: ${params.Keterangan}`,
     alternates: {
-      canonical: `/shop/${decodedNamaProduk}`,
+      canonical: `https://www.kampungperca.id/shop`,
     },
     robots: {
       index: true,
@@ -27,28 +23,16 @@ export async function generateMetadata({
     keywords: [
       "Jual Alas Mangkuk Perca",
       "Jual Appron Perca",
-      "Jual Baju Anak Perca",
       "Jual Baju Pangsi",
-      "Jual Baju Tidur Perca",
-      "Jual Bantal Duduk",
-      "Jual Bantal Kursi",
-      "Jual Blouse Perca",
       "Jual Cempal Ayam",
-      "Jual Daster",
       "Jual Dompet Koin",
       "Jual Gantungan Kunci",
       "Jual Goodie Bag Perca",
-      "Jual Ikat Rambut",
-      "Jual Keset Bentuk",
       "Jual Masker Perca",
       "Jual Lap tangan",
       "Jual Outer Perca",
-      "Jual Pouch",
-      "Jual Rok Perca",
       "Jual Rompi Perca",
-      "Jual Baju Tidur Perca",
       "Jual Totopong",
-      "Jual Notebook",
       "Jual Tempat tisu perca",
       "Jual Baju Tidur Perca",
       "Jual Tas Perca",
