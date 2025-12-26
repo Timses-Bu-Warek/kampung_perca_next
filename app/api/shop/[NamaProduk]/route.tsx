4bec51c15e05b06391254d3d034e2a72bf783a8a
Michael Mervin Ruswan <michaelruswan@gmail.com>
2025-12-26 10:58:28 +0700

    refactor: improve type definitions and formatting in route.tsx

--- app/api/shop/[NamaProduk]/route.tsx
+++ app/api/shop/[NamaProduk]/route.tsx
@@ -8,40 +8,44 @@ import { NextRequest, NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 
 // export async function GET(request : Request, context : { params : {id: number} }) {
-export async function GET(request : Request, context : { params : {NamaProduk: string} }) {
+export async function GET(
+  request: Request,
+  context: { params: Promise<{ NamaProduk: string }> }
+) {
   // const { searchParams } = new URL(request.url);
   // const API_SECRET_KEY = searchParams.get("API_SECRET_KEY")
   // if(API_SECRET_KEY !== process.env.API_SECRET_KEY) {
   //   return new Response("Not Authorized", { status: 500 })
   // }
-  const origin = request.headers.get('origin')
-  
+  const origin = request.headers.get("origin");
+
   try {
     // const id = req.query.id;
-    const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+    const id = request.url.slice(request.url.lastIndexOf("/") + 1);
     const client = await connectToDatabase();
     const db = client.db("KampungPercaDB");
     // console.log(id);
     // console.log(context);
-    const NamaProduk = context.params.NamaProduk;
+    const { NamaProduk } = await context.params;
     // console.log(NamaProduk);
 
-  
-    const oneProduct = await db.collection("products").findOne({ NamaProduk: NamaProduk  });
+    const oneProduct = await db
+      .collection("products")
+      .findOne({ NamaProduk: NamaProduk });
     // console.log(oneProduct)
     // return new Response(JSON.stringify(allProducts), { status: 200 });
-    if(oneProduct) {
+    if (oneProduct) {
       return new NextResponse(JSON.stringify(oneProduct), {
         headers: {
-            'Access-Control-Allow-Origin': origin || "*",
-            'Content-Type': 'application/json',
-        }
-    })
+          "Access-Control-Allow-Origin": origin || "*",
+          "Content-Type": "application/json",
+        },
+      });
     } else {
-      return NextResponse.json({message: "Gagal memuat produk", status: 500})
+      return NextResponse.json({ message: "Gagal memuat produk", status: 500 });
     }
   } catch (error) {
     // console.log(error);
-    return NextResponse.json({message: "Cannot make connection"})
+    return NextResponse.json({ message: "Cannot make connection" });
   }
 }
