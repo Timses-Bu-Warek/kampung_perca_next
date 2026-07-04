f345b8027477cdb72b221eb5e178b478d0f8dbe9
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 20:16:58 +0700

    refactor: change import to type import for CldImageProps in CldImage.tsx

--- app/components/CldImage/CldImage.tsx
+++ app/components/CldImage/CldImage.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { CldImage as NextCldImage, CldImageProps } from 'next-cloudinary';
+import { CldImage as NextCldImage, type CldImageProps } from "next-cloudinary";
 
 const CldImage = (props: CldImageProps) => {
-  return <NextCldImage {...props} />
-}
+  return <NextCldImage {...props} />;
+};
 
 export default CldImage;
