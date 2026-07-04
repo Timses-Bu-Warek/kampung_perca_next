1820d5f2a4c554a9f41a4728701433cdf1d2d448
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 18:58:32 +0700

    feat: add environment configuration for client and server with Cloudinary and MongoDB settings

---
+++ lib/env/server.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+	server: {
+		CLOUDINARY_API_KEY: z.string().min(1),
+		CLOUDINARY_API_SECRET: z.string().min(1),
+		MONGODB_URI: z.string().min(1),
+		MONGODB_ATLAS_PROJECT_ID: z.string().min(1),
+		MONGODB_ATLAS_CLUSTER: z.string().min(1),
+		MONGODB_ATLAS_PUBLIC_KEY: z.string().min(1),
+		MONGODB_ATLAS_PRIVATE_KEY: z.string().min(1),
+		API_SECRET_KEY: z.string().min(1),
+		MONGODB_API_KEY: z.string().min(1),
+		BASE_URL: z.string().min(1),
+	},
+	experimental__runtimeEnv: process.env,
+});
