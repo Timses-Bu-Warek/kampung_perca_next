3cf24918c688cfbb535577b1b7eb27a5a02ae8f7
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 20:17:59 +0700

    refactor: clean up whitespace in contohApp.tsx

--- lib/contohApp.tsx
+++ lib/contohApp.tsx
@@ -1,16 +1,16 @@
 // // `app` directory
- 
+
 // // This function can be named anything
 // async function getProjects() {
 //     const res = await fetch(`https://...`, { cache: 'no-store' })
 //     const projects = await res.json()
-   
+
 //     return projects
 //   }
-   
+
 //   export default async function Dashboard() {
 //     const projects = await getProjects()
-   
+
 //     return (
 //       <ul>
 //         {projects.map((project) => (
