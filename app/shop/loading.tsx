13efd223bfb1072ef628502ad0f9a633940f797b
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 22:29:28 +0700

    fix: wrap Skeleton component in a div for proper grid layout in Loading component

--- app/shop/loading.tsx
+++ app/shop/loading.tsx
@@ -14,7 +14,9 @@ export default function Loading() {
           <Skeleton count={2} />
         </div>
         <div className="col-span-3">
-          <Skeleton count={30} />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <Skeleton count={30} />
+          </div>
         </div>
       </div>
     </main>
