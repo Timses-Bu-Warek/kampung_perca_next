3d6fe7e72c0356070339684f13a8169d01651002
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 21:02:13 +0700

    refactor: update fetch logic in getAllProducts to use server environment variable for base URL and improve error handling

--- lib/getAllProducts.tsx
+++ lib/getAllProducts.tsx
@@ -3,13 +3,17 @@ import { serverEnvironment } from "./env/server";
 export default async function getAllProducts() {
   // const API_SECRET_KEY = process.env.API_SECRET_KEY;
 
-  const res = await fetch(`${process.env["BASE_URL"]}/api/shop`, {
+  const res = await fetch(`${serverEnvironment.BASE_URL}/api/shop`, {
     next: { revalidate: 10 },
   });
 
   const products = await res.json();
 
-  if (!res.ok) throw new Error("Failed to fetch products data");
+  if (!res.ok)
+    return {
+      error: true,
+      message: "Failed to fetch products data",
+    };
 
   return products;
 }
