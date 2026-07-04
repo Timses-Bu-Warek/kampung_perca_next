95cd878c6c5d45c16120079ffe1ffc76e5783672
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 20:45:15 +0700

    refactor: simplify fetch logic in getSearchProducts function by removing environment-specific URLs

--- lib/getSearchProducts.tsx
+++ lib/getSearchProducts.tsx
@@ -1,33 +1,25 @@
-export default async function getSearchProducts(namaProdukParams: string | null, sortParams: string | null) {
-    // const { searchParams } = new URL(request.url);
-    // const nama = searchParams.get("NamaProduk")
-    if (process.env.NODE_ENV === 'production') {
-        if (namaProdukParams && !sortParams) {
-            const res = await fetch(`https://www.kampungperca.id/api/shop/search?NamaProduk=${namaProdukParams}`)
-            return responseJSON(res)
-        } else if (namaProdukParams && sortParams) {
-            const res = await fetch(`https://www.kampungperca.id/api/shop/search?NamaProduk=${namaProdukParams}&sort=${sortParams}`)
-            return responseJSON(res)
-        } else if (!namaProdukParams && sortParams) {
-            const res = await fetch(`https://www.kampungperca.id/api/shop/search?sort=${sortParams}`)
-            return responseJSON(res)
-        } 
-    } else {
-        if (namaProdukParams && !sortParams) {
-            const res = await fetch(`http://localhost:3000/api/shop/search?NamaProduk=${namaProdukParams}`)
-            return responseJSON(res)
-        } else if (namaProdukParams && sortParams) {
-            const res = await fetch(`http://localhost:3000/api/shop/search?NamaProduk=${namaProdukParams}&sort=${sortParams}`)
-            return responseJSON(res)
-        } else if (!namaProdukParams && sortParams) {
-            const res = await fetch(`http://localhost:3000/shop/search?sort=${sortParams}`)
-            return responseJSON(res)
-        } 
-    }
+export default async function getSearchProducts(
+  namaProdukParams: string | null,
+  sortParams: string | null,
+) {
+  // const { searchParams } = new URL(request.url);
+  // const nama = searchParams.get("NamaProduk")
+  if (namaProdukParams && !sortParams) {
+    const res = await fetch(`/api/shop/search?NamaProduk=${namaProdukParams}`);
+    return responseJSON(res);
+  } else if (namaProdukParams && sortParams) {
+    const res = await fetch(
+      `/api/shop/search?NamaProduk=${namaProdukParams}&sort=${sortParams}`,
+    );
+    return responseJSON(res);
+  } else if (!namaProdukParams && sortParams) {
+    const res = await fetch(`/api/shop/search?sort=${sortParams}`);
+    return responseJSON(res);
+  }
 }
 
-export async function responseJSON (res : Response) {
-    const products = await res.json()
-    if (!res.ok) throw new Error('Failed to fetch products data')
-    return products;
+export async function responseJSON(res: Response) {
+  const products = await res.json();
+  if (!res.ok) throw new Error("Failed to fetch products data");
+  return products;
 }
