e894fb4c38fa91f52b634f2a4b2f825f17ec7044
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-05 00:55:17 +0700

    fix: enhance loading component structure and styling

--- app/shop/loading.tsx
+++ app/shop/loading.tsx
@@ -18,8 +18,18 @@ export default function Loading() {
         <div className="col-span-3">
           <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {newArray.map((_, index) => (
-              <div key={index}>
-                <Skeleton height={300} />
+              <div
+                className="bg-white w-60 space-y-3 overflow-hidden rounded-md p-3 group flex justify-between flex-col shadow-xs "
+                key={index}
+              >
+                <div className="h-75 w-full rounded-lg bg-gray-200"></div>
+                <div className="space-y-3">
+                  <div className="h-5 w-8/12 rounded-full bg-gray-200"></div>
+                  <div className="space-y-1">
+                    <div className="pt-4 pb-3 px-4 bg-gray-300 w-full h-6"></div>
+                    <div className="pt-4 pb-3 px-4 bg-gray-300 w-full h-6"></div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
