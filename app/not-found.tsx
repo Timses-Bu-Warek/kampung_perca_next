691b6f7339cb91da9ee375ba3960be276a396af4
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-05 00:06:42 +0700

    refactor: standardize Link component usage and improve code formatting in not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="  flex items-center justify-center h-screen">
       <div className="  text-center">
         <h1 className="text-4xl font-semibold mb-4">Oops!</h1>
-        <p className="text-gray-600">Mohon maaf, website tujuanmu belum tersedia.</p>
-        <a className="mt-4 text-blue-500 hover:underline" href="/">
+        <p className="text-gray-600">
+          Mohon maaf, website tujuanmu belum tersedia.
+        </p>
+        <Link className="mt-4 text-blue-500 hover:underline" href="/">
           Back to Home
-        </a>
+        </Link>
       </div>
     </div>
   );
