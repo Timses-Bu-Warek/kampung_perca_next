b67294de21f6557c0cb9a07a8d3bb12988f282ff
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 21:43:11 +0700

    refactor: streamline product data handling by replacing useSWR with direct productPromise usage

--- app/shop/[NamaProduk]/components/DetailedProduct.tsx
+++ app/shop/[NamaProduk]/components/DetailedProduct.tsx
@@ -4,29 +4,22 @@ import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import Link from "next/link";
 import Image from "next/image";
-import useSWR from "swr";
 import ImageContoh from "@/public/img/Produk/contoh_baju.webp";
-import { type SetStateAction, useState } from "react";
+import { type SetStateAction, use, useState } from "react";
 import Breadcrumbs from "@/app/components/breadcrumbs";
 import QuantityComp from "./QuantityComp";
 import SizeComp from "./SizeComp";
 import CldImage from "@/app/components/CldImage";
-import { serverEnvironment } from "@/lib/env/server";
-
-const fetcher = async (address: string) => {
-  const response = await fetch(
-    address + "?API_SECRET_KEY=" + serverEnvironment.API_SECRET_KEY,
-  );
-  // const data = await response.json()
-  // return data
-  return response.json();
-};
+import type getProduct from "@/lib/getProduct";
 
 export default function DetailedProduct({
   productName,
+  productPromise,
 }: Readonly<{
   productName: string;
+  productPromise: ReturnType<typeof getProduct>;
 }>) {
+  const product = use(productPromise);
   const [quantity, setQuantity] = useState(1);
 
   const chooseQuantity = (quantity: SetStateAction<number>) => {
@@ -38,17 +31,7 @@ export default function DetailedProduct({
     setUkuran(ukuran);
   };
 
-  // const fetcher = getProduct(NamaProduk)
-  // const fetcher = async(url: string) => {
-  //      fetch(url).then(r => r.json())
-  // }
-
-  const address = `/api/shop/${productName}`;
-  // const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error } = useSWR(address, fetcher);
-
-  if (error) return <div>Failed to fetch product</div>;
-  if (!data)
+  if (!product)
     return (
       <div>
         <Skeleton />
@@ -56,7 +39,7 @@ export default function DetailedProduct({
     );
 
   let waAPI;
-  if (data.Ukuran) {
+  if (product["Ukuran"]) {
     waAPI =
       "https://api.whatsapp.com/send/?phone=6285810096563&text=Hai kak, aku mau pesan : " +
       quantity +
@@ -82,7 +65,7 @@ export default function DetailedProduct({
       <div className="container flex items-center gap-4 py-4">
         <Breadcrumbs
           params={{
-            slug: data.NamaProduk,
+            slug: product["NamaProduk"],
           }}
         />
       </div>
@@ -91,13 +74,13 @@ export default function DetailedProduct({
       {/* <!-- product view --> */}
       <div className="container flex flex-col gap-6 md:grid md:grid-cols-4">
         <div className="md:col-span-2">
-          {data.FotoProduk ? (
+          {product["FotoProduk"] ? (
             <CldImage
               width={500}
               loading="lazy"
               height={500}
-              src={data.FotoProduk}
-              alt={data.NamaProduk}
+              src={product["FotoProduk"]}
+              alt={product["NamaProduk"]}
               className="w-full min-h-[200px]"
             />
           ) : (
@@ -150,7 +133,7 @@ export default function DetailedProduct({
         {/* <!-- produk konten --> */}
         <div className="md:col-span-2">
           <h2 className="mb-2 text-3xl font-semibold uppercase font-montserrat">
-            {data.NamaProduk}
+            {product["NamaProduk"]}
           </h2>
           {/* <div className="flex items-center mb-3">
                         <div className="flex gap-1 text-sm text-yellow-400">
@@ -165,7 +148,7 @@ export default function DetailedProduct({
           <div className="space-y-2">
             <p className="space-x-2 font-semibold text-gray-800 font-inter">
               <span>Avilability : </span>
-              {data.FotoProduk ? (
+              {product["FotoProduk"] ? (
                 <span className="text-green-600">In Stock</span>
               ) : (
                 <span className="text-red-600">Out of Stock</span>
@@ -177,22 +160,22 @@ export default function DetailedProduct({
             </p>
             <p className="space-x-2 font-semibold text-gray-800 font-inter">
               <span>Category : </span>
-              <span className="text-gray-600">{data.Kategori}</span>
+              <span className="text-gray-600">{product["Kategori"]}</span>
             </p>
           </div>
           <div className="flex items-baseline mt-4 mb-1 space-x-2 font-inter">
             <p className="text-xl font-semibold text-primary">
-              Rp {Intl.NumberFormat("id-ID").format(data.Harga)}
+              Rp {Intl.NumberFormat("id-ID").format(product["Harga"])}
             </p>
             {/* <p className="text-base text-gray-400 line-through">Rp. 123.000</p> */}
           </div>
-          <p className="mt-4 text-gray-600">{data.Keterangan}</p>
+          <p className="mt-4 text-gray-600">{product["Keterangan"]}</p>
           <div className="grid grid-cols-2 md:block">
             {/* <!-- size --> */}
-            {data.Ukuran ? (
+            {product["Ukuran"] ? (
               <SizeComp
                 params={{
-                  dataUkuran: data.Ukuran,
+                  dataUkuran: product["Ukuran"],
                   chooseUkuran: chooseUkuran,
                 }}
               />
@@ -263,7 +246,7 @@ export default function DetailedProduct({
           Product Detail
         </h3>
         <div className="w-3/5 pt-4">
-          <div className="space-y-3 text-gray-600">{data.Keterangan}</div>
+          <div className="space-y-3 text-gray-600">{product["Keterangan"]}</div>
           {/* <!-- table detail --> */}
           {/* <table
                         className="mt-6 text-sm text-left text-gray-600 border-collapse table-auto"
