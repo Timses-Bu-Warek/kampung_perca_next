fc54cf5f5c0cd3abac7b1674252e31ac5bc781bb
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-05 00:38:55 +0700

    refactor: replace div with Link component in FloatingButton and standardize string quotes in layout.tsx

--- app/components/FloatingButton.tsx
+++ app/components/FloatingButton.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function FloatingButton() {
   return (
-    <div className="fixed z-20 bottom-24 right-6 px-3 bg-blue-600 w-auto h-10 rounded-3xl drop-shadow-lg flex justify-center font-montserrat items-center text-white text-sm hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-600 scroll-auto">
+    <Link
+      className="fixed z-20 bottom-24 right-6 px-3 bg-blue-600 w-auto h-10 rounded-3xl drop-shadow-lg flex justify-center font-montserrat items-center text-white text-sm hover:bg-blue-700 hover:drop-shadow-2xl hover:animate-bounce duration-600 scroll-auto"
+      href="#"
+    >
       <i className="fas fa-arrow-up"></i>
       <span> &nbsp; Back to Top</span>
-    </div>
+    </Link>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import "../styles/globals.css";
-import GoogleAnalytics from "@bradgarropy/next-google-analytics";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import type { Metadata } from "next";
-import Head from "next/head";
-import { cookies } from "next/headers";
-import Link from "next/link";
-import FloatingButton from "./components/FloatingButton";
-import Footer from "./footer";
-import Header from "./header";
-import "../styles/fontawesome-free-6.5.2-web/css/fontawesome.css";
-import "../styles/fontawesome-free-6.5.2-web/css/brands.css";
-import "../styles/fontawesome-free-6.5.2-web/css/solid.css";
-import { serverEnvironment } from "@/lib/env/server";
-import WhatsAppFloatingButton from "./components/WhatsAppFloatingButton";
-import "react-loading-skeleton/dist/skeleton.css";
+import '../styles/globals.css';
+import GoogleAnalytics from '@bradgarropy/next-google-analytics';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import type { Metadata } from 'next';
+import Head from 'next/head';
+import { cookies } from 'next/headers';
+import Link from 'next/link';
+import FloatingButton from './components/FloatingButton';
+import Footer from './footer';
+import Header from './header';
+import '../styles/fontawesome-free-6.5.2-web/css/fontawesome.css';
+import '../styles/fontawesome-free-6.5.2-web/css/brands.css';
+import '../styles/fontawesome-free-6.5.2-web/css/solid.css';
+import { serverEnvironment } from '@/lib/env/server';
+import WhatsAppFloatingButton from './components/WhatsAppFloatingButton';
+import 'react-loading-skeleton/dist/skeleton.css';
 // import ReactModal from 'react-modal'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -24,23 +24,23 @@ export const metadata: Metadata = {
     canonical: serverEnvironment.BASE_URL,
   },
   description:
-    "Kampung Perca adalah desa wisata kampung tematik di Sindangsari, Kota Bogor. Desa ini memproduksi kerajinan unik dari kain perca yang penuh warna, mempromosikan budaya lokal dan kreativitas masyarakat.",
+    'Kampung Perca adalah desa wisata kampung tematik di Sindangsari, Kota Bogor. Desa ini memproduksi kerajinan unik dari kain perca yang penuh warna, mempromosikan budaya lokal dan kreativitas masyarakat.',
   keywords: [
-    "Kampung Perca",
-    "kerajinan dari kain",
-    "kerajinan dari kain perca",
-    "kerajinan kain perca",
-    "barang dari kain perca",
-    "contoh kerajinan dari kain",
-    "contoh kerajinan dari kain perca",
-    "contoh kerajinan kain perca",
-    "gambar kerajinan kain perca",
-    "kain perca kerajinan",
-    "kerajinan dari bahan kain perca",
+    'Kampung Perca',
+    'kerajinan dari kain',
+    'kerajinan dari kain perca',
+    'kerajinan kain perca',
+    'barang dari kain perca',
+    'contoh kerajinan dari kain',
+    'contoh kerajinan dari kain perca',
+    'contoh kerajinan kain perca',
+    'gambar kerajinan kain perca',
+    'kain perca kerajinan',
+    'kerajinan dari bahan kain perca',
   ],
-  metadataBase: new URL("https://kampungperca.id/"),
+  metadataBase: new URL('https://kampungperca.id/'),
   openGraph: {
-    title: "Kampung Perca Sindangsari Kota Bogor",
+    title: 'Kampung Perca Sindangsari Kota Bogor',
   },
   // instruksi untuk pengindeksan oleh mesin pencari,
   robots: {
@@ -49,8 +49,8 @@ export const metadata: Metadata = {
     nocache: true,
   },
   title: {
-    default: "Desa Wisata Kampung Perca Kota Bogor",
-    template: "%s - Kampung Perca",
+    default: 'Desa Wisata Kampung Perca Kota Bogor',
+    template: '%s - Kampung Perca',
   },
 };
 
@@ -60,7 +60,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const cookiesBukuTamu = await cookies();
-  const isFilled = cookiesBukuTamu.has("isBukuTamu");
+  const isFilled = cookiesBukuTamu.has('isBukuTamu');
   return (
     <>
       <html lang="en">
@@ -78,12 +78,10 @@ export default async function RootLayout({
           <Header />
           {/* {isFilled === true ? "" : <ModalBukuTamu />} */}
           {children}
-          <Link href="#">
-            <FloatingButton />
-          </Link>
+          <FloatingButton />
           <Link
             href={
-              "https://api.whatsapp.com/send/?phone=6285810096563&text=Hai kak, aku mau tanya seputar produk Kampung Perca"
+              'https://api.whatsapp.com/send/?phone=6285810096563&text=Hai kak, aku mau tanya seputar produk Kampung Perca'
             }
           >
             <WhatsAppFloatingButton />
