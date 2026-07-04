6dc9e81662a36c97ee03da435b73a09d93d23dc4
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 22:25:14 +0700

    feat: add Loading component with skeleton loading UI

---
+++ app/shop/loading.tsx
@@ -0,0 +1,22 @@
+import Skeleton from "react-loading-skeleton";
+import Breadcrumbs from "../components/breadcrumbs";
+
+export default function Loading() {
+  return (
+    <main>
+      <Breadcrumbs
+        params={{
+          slug: "Shop",
+        }}
+      />
+      <div className="container grid items-start gap-6 pt-4 pb-16 md:grid-cols-4">
+        <div className="col-span-1 px-4 pb-6 bg-white rounded-sm shadow-sm">
+          <Skeleton count={2} />
+        </div>
+        <div className="col-span-3">
+          <Skeleton count={30} />
+        </div>
+      </div>
+    </main>
+  );
+}
