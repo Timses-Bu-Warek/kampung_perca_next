94494f4cc9d7a093104b4dd1e491e05ef6379f19
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-04 18:52:37 +0700

    refactor: update base URL handling in sitemap function

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,51 +1,51 @@
 import getAllProducts from "@/lib/getAllProducts";
 
 export default async function sitemap() {
-  const baseUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://kampungperca.id/"
-      : "http://localhost:3000/";
+	const baseUrl =
+		process.env.NODE_ENV === "production"
+			? process.env.BASE_URL
+			: "http://localhost:3000/";
 
-  //Get All Post
-  const product = await getAllProducts();
-  // console.log(product);
-  const productUrls =
-    product?.map((products: { NamaProduk: any }) => {
-      return {
-        url: `${baseUrl}shop/${products.NamaProduk}`,
-        lastModified: new Date(),
-      };
-    }) ?? [];
+	//Get All Post
+	const product = await getAllProducts();
+	// console.log(product);
+	const productUrls =
+		product?.map((products: { NamaProduk: any }) => {
+			return {
+				url: `${baseUrl}shop/${products.NamaProduk}`,
+				lastModified: new Date(),
+			};
+		}) ?? [];
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-    },
-    {
-      url: "https://kampungperca.id/shop",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://kampungperca.id/about-us",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://kampungperca.id/sudut-edukasi",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.kampungperca.id/sudut-edukasi/potensi-kain-perca",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.kampungperca.id/sudut-edukasi/dampak-positif-kain-perca",
-      lastModified: new Date(),
-    },
-    {
-      url: "https://www.kampungperca.id/sudut-edukasi/manfaat-kain-perca",
-      lastModified: new Date(),
-    },
-    ...productUrls,
-  ];
+	return [
+		{
+			url: baseUrl,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/shop`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/about-us`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/sudut-edukasi`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/sudut-edukasi/potensi-kain-perca`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/sudut-edukasi/dampak-positif-kain-perca`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/sudut-edukasi/manfaat-kain-perca`,
+			lastModified: new Date(),
+		},
+		...productUrls,
+	];
 }
