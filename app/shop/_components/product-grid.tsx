85a8e07d6d934dfb50bb0e9e77452f56494e458a
Michael Mervin Ruswan <michaelruswan@gmail.com>
2026-07-05 22:27:54 +0700

    refactor: import Image component from next/image and CldImage for improved image handling

--- app/shop/_components/product-grid.tsx
+++ app/shop/_components/product-grid.tsx
@@ -1,5 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
+import Image from 'next/image';
 import Link from 'next/link';
+import CldImage from '@/app/_components/cloudinary/cld-image';
 import type getSearchProducts from '@/features/product/data-access/get-search-products';
 import ImageContoh from '@/public/img/Produk/contoh_baju.webp';
 
